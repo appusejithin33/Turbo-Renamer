e826867bfbb2de386eb22e6be5038293d12a52b4
princedavitt <64537102+princedavitt@users.noreply.github.com>
2020-09-26 23:28:49 +0400

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """Vanakkam <i><b>{}</b></i>,
+    START_TEXT = """നമസ്കാരം<i><b>{}</b></i>,
 
 Welcome to Our <a href='https://t.me/turboremaxbot'><b>Turbo Renamer ⚡</b></a> bot by {}
 
@@ -13,8 +13,8 @@ Type /help for more details."""
     DOWNLOAD_START = "Downloading to Turbo Server.....⤵️"
     UPLOAD_START_VIDEO = "Uploading as Video.....⤴️"
     UPLOAD_START = "Uploading as File.....⤴️"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Turbo Renamer](https://t.me/avatharsink)'s bot.**"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [Turbo Renamer](https://t.me/princedavitt)'s bot.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://t.me/avatharsink'>Turbo Renamer</a>"
@@ -22,12 +22,12 @@ Type /help for more details."""
     DEL_ETED_CUSTOM_THUMB_NAIL = "🤔 Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "🤨 Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "👹 Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = "<b>\nShare and Support \n\n@SerialCoIn</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>\nShare and Support \n\n@Cinemaglitz</b>"
     NO_CUSTOM_THUMB_NAIL_FOUND = "😠 No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Vanakkam <b><i>{}</i></b>, 
 
-I am Turbo Renamer bot ✍ by <a href='https://t.me/{}'><b>Asuran</b></a>
+I am Turbo Renamer bot ✍ by <a href='https://t.me/{}'><b>PrinceDavitt</b></a>
     
 1. Send Me A Thumbnail.
 
@@ -38,11 +38,11 @@ I am Turbo Renamer bot ✍ by <a href='https://t.me/{}'><b>Asuran</b></a>
 4. Reply to that message with <code>/rename_vidoe new name.extension</code>. with custom thumbnail support.\n(uploading as Video)
 
    
-<b>Thanks to <i><a href="https://t.me/avatharsink">Turbo Renamer 🙇</a></i> for his source code. check /about for source code</b>
+<b>Thanks to <i><a href="https://t.me/Princedavitt">CG Renamer 🙇</a></i> for his source code. check /about for source code</b>
 
 --------
 
-Support Group : <b>@SerialCoIn</b>"""
+Support Group : <b>@Cglitz</b>"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "🙆 Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
     REPLY_TO_DOC_FOR_RENAME_VIDEO = "🙋 Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
@@ -61,10 +61,10 @@ Please short your file name and try again!"""
 
 **🧰 Framework:** Pyrogram
 
-**👨‍💻 Developer:** [Turbo Renamer](https://t.me/turboremaxbot)
+**👨‍💻 Developer:** [Turbo Renamer](https://t.me/Princedavitt)
 
-📮 Channel: [Serial](https://t.me/SerialCoIn)
+📮 Channel: [Serial](https://t.me/Cinemaglitz)
 
-**👥 Group:** [Tamil Serial Group](https://t.me/SerialCoIng)
+**👥 Group:** [MAL MOVIE Group](https://t.me/Cinemaglitz)
 
-**💻 Source Code:**[Press Me](https://t.me/SerialCoIn)"""
+
